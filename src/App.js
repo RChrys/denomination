8cfbfd3fbe31127eb975f8f6b66b221664d04971
RChrys <christefen.dyvrande@gmail.com>
2020-05-20 17:34:32 +0200

    App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Denomination from './Components/Denomination'
 const denominations = [1, 5, 10, 20, 50, 100, 200] ;
 
 function App() {
-  console.log(33%50)
   return (
     <Denomination denominations={denominations} />
   );
